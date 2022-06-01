f2eb3237a1cb995e47f9289ba36b2a57bb173a10
akn5 <chengp.alain@gmail.com>
2022-06-01 11:19:14 +0800

    server.js

--- client/client.js
+++ client/client.js
@@ -3,3 +3,12 @@ const PORT = 8005;
 const HOST = '127.0.0.1';
 
 // Client
+const client = dgram.createSocket('udp4');
+const msg = "How are you!";
+
+client.send(msg, PORT, HOST, (err) => {
+    if (err) throw err;
+
+    console.log('UDP message sent');
+    client.close();
+});
