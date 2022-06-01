659ac71c3349737a8d39b72aa6c50f0b879c4f01
akn5 <104754302+alain-cheng@users.noreply.github.com>
2022-06-01 11:20:46 +0800

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,14 +1,1 @@
-const dgram = require('dgram');
-const PORT = 8005;
-const HOST = '127.0.0.1';
 
-// Client
-const client = dgram.createSocket('udp4');
-const msg = "How are you!";
-
-client.send(msg, PORT, HOST, (err) => {
-    if (err) throw err;
-
-    console.log('UDP message sent');
-    client.close();
-});
